baa9dc4076dad804fcd9f324b1699816ef506f69
felipejoq <felipejofreq@gmail.com>
2023-07-20 19:30:06 -0400

    Agent added to fetch function

--- helpers/holidays.js
+++ helpers/holidays.js
@@ -1,27 +1,31 @@
 const rootUrl = 'https://apis.digital.gob.cl/fl/feriados'
 const date = new Date();
+const https = require('https');
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
 const isHolidayToday = async () => {
     const source = `${rootUrl}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
-    const response = await fetch(source);
+    const response = await fetch(source, {agent});
     return await response.json();
 }
 
 const wasYesterdayHoliday = async () => {
     const source = `${rootUrl}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate() - 1}`;
-    const response = await fetch(source);
+    const response = await fetch(source, {agent});
     return await response.json();
 }
 
 const tomorrowIsHoliday = async () => {
     const source = `${rootUrl}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate() + 1}`;
-    const response = await fetch(source);
+    const response = await fetch(source, {agent});
     return await response.json();
 }
 
 const nextHoliday = async () => {
     const source = `${rootUrl}/${date.getFullYear()}`;
-    const response = await fetch(source);
+    const response = await fetch(source, {agent});
     const data = await response.json();
     return data.find(holiday => Date.parse(holiday.fecha) > date);
 }
