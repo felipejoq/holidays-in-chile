3b8f926adeb5900cf3a4510d75d18835d00b4f3f
felipejoq <felipejofreq@gmail.com>
2023-07-24 20:18:44 -0400

    Timezone America/Santiago  added to date today

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,11 +1,22 @@
 const helper = {};
 
-helper.toJSON =  (context) => {
+helper.toJSON = (context) => {
     return JSON.stringify(context);
 };
 
 helper.isInalienable = (value) => {
     return value === 0 ? 'Si' : 'No';
+}
+
+helper.todayIs = () => {
+    return new Date().toLocaleDateString("es-CL",
+        {
+            weekday: "long",
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+            timeZone: 'America/Santiago'
+        });
 }
 
 module.exports = helper;
--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,13 @@ const {isHolidayToday, tomorrowIsHoliday, wasYesterdayHoliday, nextHoliday} = re
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const date = new Date().toLocaleDateString("es-CL", {
-        weekday: "long",
-        day: "2-digit",
-        month: "long",
-        year: "numeric"
-    });
 
     const today = await isHolidayToday();
     const tomorrow = await tomorrowIsHoliday();
     const yesterday = await wasYesterdayHoliday();
     const next = await nextHoliday();
 
-    res.render('main', {today, tomorrow, yesterday, next, date});
+    res.render('main', {today, tomorrow, yesterday, next});
 });
 
 module.exports = router;
