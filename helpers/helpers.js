e717ee7457e64191367f591824af25325eb187dc
felipejoq <felipejofreq@gmail.com>
2023-08-15 11:40:11 -0400

    Helper parse date added and styles modified

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -19,4 +19,19 @@ helper.todayIs = () => {
         });
 }
 
+helper.dateParse = (dateString) => {
+
+    let dateSplit = dateString.split('-').map(value => Number(value))
+    let date = new Date(Date.UTC(dateSplit[0], dateSplit[1] - 1, dateSplit[2] + 1))
+
+    return date.toLocaleDateString("es-CL",
+        {
+            weekday: "long",
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+            timeZone: 'America/Santiago'
+        });
+}
+
 module.exports = helper;
